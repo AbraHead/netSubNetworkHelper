b594849fd34ce0195417b2358f813899c78d2152
Виталий Викторович Вишневский <vishnevsky@saut.ru>
2023-04-13 17:37:59 +0500

    Initial commit

---
+++ SubNetworkHelper/SubNetworkHelper.cs
@@ -0,0 +1,171 @@
+﻿namespace SubNetworkHelper;
+using System.Net;
+
+public class SubNetworkHelper
+    {
+        private byte[] _ipBytes;
+        private byte[] _maskBytes;
+        private byte[] _dnsBytes = new byte[]{8,8,8,8};
+
+        /*public byte[] IpBytes => _ipBytes;
+        public byte[] MaskBytes => _maskBytes;
+        public byte[] DnsBytes => _dnsBytes;*/
+
+        private byte[] _startIPAddress;
+        public IPAddress StartIpAddress => new IPAddress(_startIPAddress);
+        
+        private byte[] _endIPAddress;
+        public IPAddress EndIpAddress => new IPAddress(_endIPAddress);
+
+        private byte[][] _networkRange;
+        public IPAddress[] NetworkRange
+        {
+            get
+            {
+                return new IPAddress[] {new IPAddress(_networkRange[0]),
+                                        new IPAddress(_networkRange[1])};
+            }
+        } 
+
+        private byte[] _networkIP;
+        public IPAddress NetworkIp => new IPAddress(_networkIP);
+
+        private byte[] _wildcardIP;
+        public IPAddress WildcardIp => new IPAddress(_wildcardIP);
+
+        private byte[] _broadcastIP;
+        public IPAddress BroadcastIp => new IPAddress(_broadcastIP);
+
+        /*public static byte lastOctetNumber = 4;
+        public static byte arrayLastOctetNumber = (byte)(lastOctetNumber - 1);*/
+        public SubNetworkHelper(IPAddress ip, IPAddress mask, IPAddress dns)
+        {
+            _ipBytes = ip.GetAddressBytes();
+            _maskBytes = mask.GetAddressBytes();
+            _dnsBytes = dns.GetAddressBytes();
+            _networkIP = new byte[4];
+            _wildcardIP = new byte[4];
+            _broadcastIP = new byte[4];
+            _startIPAddress = new byte[4];
+            _endIPAddress = new byte[4];
+            
+            OctetHelper octet;
+            int octetindexer = 0;
+            bool lastOctet = false;
+            foreach (var (ipoctet, maskoctet) in Enumerable.Zip(_ipBytes, _maskBytes))
+            {
+                // Флаг требуется для проверки последний ли октет рассматривается
+                // Умираю...
+                lastOctet = octetindexer == 3;
+                
+                octet = new OctetHelper(ipoctet, maskoctet);
+                _networkIP[octetindexer] = octet.NetworkOctet;
+                _wildcardIP[octetindexer] = octet.WildcardOctet;
+                _broadcastIP[octetindexer] = octet.BroadcastOctet;
+                _startIPAddress[octetindexer] = octet.GetMinHostOctet(lastOctet);
+                _endIPAddress[octetindexer] = octet.GetMaxHostOctet(lastOctet);
+                octetindexer++;
+            }
+
+            _networkRange = new byte[2][] { _startIPAddress, _endIPAddress };
+
+        }
+
+        /*public bool MaskValidation(byte[] mask)
+        {
+            
+        }*/
+        
+        /*public bool MaskValidation(IPAddress mask)
+        {
+            bool validmask = true;
+            OctetHelper octet;
+            foreach (var maskoctet in _maskBytes)
+            {
+                octet = new OctetHelper(maskoctet);
+                validmask = validmask & octet.ValidateMaskOctet();
+            }
+
+            return validmask;
+        }*/
+        
+        public IPAddress[] GetIPRange()
+        {
+            return new IPAddress[]
+            {
+                new IPAddress(_startIPAddress),
+                new IPAddress(_endIPAddress)
+            };
+        }
+    }
+    
+    public class OctetHelper
+    {
+        public static byte _ipOctet;
+        public static byte _maskOctet;
+        private static byte _networkOctet;
+        private static byte _wildcardOctet;
+        private static byte _broadcastOctet;
+        
+        public OctetHelper(byte ipOctet, byte maskOctet)
+        {
+            _ipOctet = ipOctet;
+            _maskOctet = maskOctet;
+            _networkOctet = (byte)(_ipOctet & _maskOctet);
+            _wildcardOctet = (byte)~_maskOctet;
+            _broadcastOctet = (byte)(_networkOctet | _wildcardOctet);
+        }
+        
+        public OctetHelper(byte maskOctet)
+        {
+            //_ipOctet = ipOctet;
+            _maskOctet = maskOctet;
+            //_networkOctet = (byte)(_ipOctet & _maskOctet);
+            //_wildcardOctet = (byte)~_maskOctet;
+            //_broadcastOctet = (byte)(_networkOctet | _wildcardOctet);
+        }
+        
+
+        //public static byte NetworkOctet => _networkOctet;
+
+        public byte NetworkOctet
+        {
+            get => _networkOctet;
+        }
+
+        public byte WildcardOctet
+        {
+            get => _wildcardOctet;
+        }
+        
+        public byte BroadcastOctet
+        {
+            get => _broadcastOctet;
+        }
+        
+        
+        public byte GetMinHostOctet(bool lastOctet)
+        {
+            return lastOctet ? (byte)(_networkOctet + (byte)1) : (byte)(_networkOctet);
+        }
+        public byte GetMaxHostOctet(bool lastOctet)
+        {
+            return lastOctet ? (byte)(_broadcastOctet - (byte)1) : (byte)(_broadcastOctet);
+        }
+
+        public bool ValidateMaskOctet()
+        {
+            bool valid = false;
+            byte startnumber = 127;
+            byte validoctet;
+            //List<byte> validoctets = new List<byte>();
+            while (startnumber != 255)
+            {
+                validoctet = (byte)~startnumber;
+                valid = valid | (_maskOctet == validoctet);
+                startnumber = (byte)(startnumber >> 1);
+            }
+            //startoctet >> 1;
+            return valid;
+        }
+    }
---
+++ SubNetworkHelper/SubNetworkHelperInt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Net;
+using System.Numerics;
+
+namespace SubNetworkHelper;
+
+
+
+public class SubNetworkHelperInt
+{
+    public IPAddress Mask { get; }
+    public IPAddress IP { get; }
+    public IPAddress Wildcard { get; }
+    public IPAddress Network { get; }
+    public IPAddress Broadcast { get; }
+    
+    public IPAddress FirstIP { get; }
+    public IPAddress EndIP { get; }
+    
+    public SubNetworkHelperInt(IPAddress ip, IPAddress mask)
+    {
+        Mask = mask;
+        IP = ip;
+        
+        byte[] bmask = mask.GetAddressBytes();
+        byte[] bip = ip.GetAddressBytes();
+        uint uintmask = BitConverter.ToUInt32(bmask);
+        uint uintip = BitConverter.ToUInt32(bip);
+        
+        uint uintwildcard = ~uintmask;
+        byte[] bwildcard = BitConverter.GetBytes(uintwildcard);
+        Wildcard = new IPAddress(bwildcard);
+
+        uint uintnetwork = uintip & uintmask;
+        byte[] bnetwork = BitConverter.GetBytes(uintnetwork);
+        Network = new IPAddress(bnetwork);
+
+        uint uintbroadcast = uintnetwork | uintwildcard;
+        byte[] bbroadcast = BitConverter.GetBytes(uintbroadcast);
+        Broadcast = new IPAddress(bbroadcast);
+
+        byte[] bfirstip = bnetwork;
+        bfirstip[^1] = (byte)(bfirstip[^1] + 1);
+        FirstIP = new IPAddress(bfirstip);
+
+        byte[] bendip = bbroadcast;
+        bendip[^1] = (byte)(bendip[^1] - 1);
+        EndIP = new IPAddress(bendip);
+
+    }
+
+    /*public static void main()
+    {
+        IPAddress ip = new IPAddress(new byte[] {192, 168, 36, 36});
+        IPAddress mask = new IPAddress(new byte[] {255, 252, 0, 0});
+        SubNetworkHelperInt test = new SubNetworkHelperInt(ip, mask);
+    }*/
+}
